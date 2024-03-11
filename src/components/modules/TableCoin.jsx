483495f1bb521ba05a12624b039cde8f53388abb
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-03-11 13:28:44 +0330

    Adding css file to the price table

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -3,12 +3,14 @@ import { RotatingLines } from "react-loader-spinner";
 
 import chartUp from "../../assets/chart-up.svg" ;
 import chartDown from "../../assets/chart-down.svg" ;
+import styles from "./TableCoin.module.css"
 
 function TableCoin({coins, isLoading}) {
    
   return (
-    <div>
-   {isLoading? ( <RotatingLines  strokeColor="#3874ff" strokeWidth="2"/>) : (   <table>
+    <div className={styles.container}>
+   {isLoading? ( <RotatingLines  strokeColor="#3874ff" strokeWidth="2"/>) : (  
+     <table className={styles.table}>
         <thead>
               <tr>
                 <th>Coin</th>
@@ -34,14 +36,14 @@ const TableRow = ( {coin :{name, image, symbol ,total_volume,current_price, pric
   return (
     <tr >
             <td>
-              <div>
+              <div className={styles.symbol}>
                 <img src={image} alt="" />
                 <span>{symbol.toUpperCase()}</span>
               </div>
             </td>
             <td>{name}</td>
             <td>{current_price.toLocaleString()}</td>
-            <td>{price_change.toFixed(2)}</td>
+            <td className={price_change >0 ? styles.success : styles.error}>{price_change.toFixed(2)}</td>
             <td>{total_volume.toLocaleString()}</td>
             <td><img src={price_change >0 ?chartUp : chartDown} alt={name} /></td>
           </tr>
