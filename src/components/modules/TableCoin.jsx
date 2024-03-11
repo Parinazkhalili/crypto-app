c73b103725fc8aa8560aa8164f1f713fed32bdc8
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-03-11 11:56:23 +0330

    Extracting the tableRow component

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -5,7 +5,7 @@ import chartUp from "../../assets/chart-up.svg" ;
 import chartDown from "../../assets/chart-down.svg" ;
 
 function TableCoin({coins}) {
-   console.log(coins)
+   
   return (
     <div>
       <table>
@@ -20,23 +20,30 @@ function TableCoin({coins}) {
               </tr>
         </thead>
         <tbody>
-          {coins.map(coin => <tr key={coin.id}>
-            <td>
-              <div>
-                <img src={coin.image} alt="" />
-                <span>{coin.symbol.toUpperCase()}</span>
-              </div>
-            </td>
-            <td>{coin.name}</td>
-            <td>{coin.current_price.toLocaleString()}</td>
-            <td>{coin.price_change_percentage_24h.toFixed(2)}</td>
-            <td>{coin.total_volume.toLocaleString()}</td>
-            <td><img src={coin.price_change_percentage_24h>0 ?chartUp : chartDown} alt={coin.name} /></td>
-          </tr>)}
+          {coins.map((coin) => (<TableRow  coin={coin} key={coin.id}/> ))}
         </tbody>
       </table>
     </div>
   )
 }
 
-export default TableCoin
+export default TableCoin ;
+
+const TableRow = ( {coin :{name, image, symbol ,total_volume,current_price, price_change_percentage_24h:price_change}}) =>{
+
+  return (
+    <tr >
+            <td>
+              <div>
+                <img src={image} alt="" />
+                <span>{symbol.toUpperCase()}</span>
+              </div>
+            </td>
+            <td>{name}</td>
+            <td>{current_price.toLocaleString()}</td>
+            <td>{price_change.toFixed(2)}</td>
+            <td>{total_volume.toLocaleString()}</td>
+            <td><img src={price_change >0 ?chartUp : chartDown} alt={name} /></td>
+          </tr>
+  )
+}
