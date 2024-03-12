8cc2f5dd1e3ab6cdd76c631070809bcac93284e5
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-03-12 12:46:50 +0330

    Adding search component and currency changes related to data

--- src/components/templates/HomePages.jsx
+++ src/components/templates/HomePages.jsx
@@ -2,30 +2,32 @@ import {useEffect, useState} from "react" ;
 import TableCoin from '../modules/TableCoin';
 import { getCoinList } from '../../services/cryptoApi';
 import Pagination from "../modules/Pagination";
+import Search from "../modules/Search";
 
 
 
 function HomePages() {
     const[coins , setCoins] = useState([]) ;
     const [isLoading , setIsLoading] = useState(true) ;
-    const [page, setPage]= useState(1)
+    const [page, setPage]= useState(1) ;
+    const [currency, setCurrency] = useState("usd")
 
 useEffect(() => {
      
    const getData = async () => {
     setIsLoading(true);
-    const res = await fetch(getCoinList(page));
+    const res = await fetch(getCoinList(page , currency));
     const json = await res.json();
     setCoins(json);
     setIsLoading(false) ;
    };
    getData();
 
-},[page]) ;
+},[page, currency]) ;
 
      return (
     <div>
-    
+       <Search  currency={currency} setCurrency={setCurrency}/>
       <TableCoin coins={coins}  isLoading={isLoading}/>
       <Pagination  page={page} setPage={setPage}/>
       </div>
