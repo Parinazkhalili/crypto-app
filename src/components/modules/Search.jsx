ede30c552097d4654ff790ab8c8a53abc96d969a
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-03-12 14:45:23 +0330

    Adding clean up function to the project

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -8,18 +8,33 @@ function Search({currency, setCurrency}) {
    
 
     useEffect(() => {
-
+        const controller = new AbortController();
         if(!text) return;
+
+    
         const search = async () => {
-            const res = await fetch(searchCoin(text));
+
+            try {
+                const res = await fetch(searchCoin(text), {signal: controller.signal});
             const json = await res.json();
             console.log(json)
-           if (json.coins)  setCoins(json.coins)
+           if (json.coins) 
+           { setCoins(json.coins)  } else 
+           {alert(json.status.error_message);}
+            } catch (error) {
+            
+                if (error.name !== "AbortError" ) {
+                    alert(error.message)
+
+                }
+                
+            }
+          
        
         }
 
        search();
-
+        return () => controller.abort() ;
     }, [text])
   return (
     <div>
