3ab47225bb37bad6a2f59eee7109bfafdbadd1b4
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-03-16 13:14:43 +0330

    Adding style to the search component

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import { searchCoin } from '../../services/cryptoApi';
 import { RotatingLines } from 'react-loader-spinner';
+import styles from  "./Search.module.css" ;
 
 function Search({currency, setCurrency}) {
     const [text, setText] = useState("");
@@ -48,14 +49,14 @@ function Search({currency, setCurrency}) {
         return () => controller.abort() ;
     }, [text])
      return (
-    <div>
+    <div className={styles.searchBox}>
         < input type="text" placeholder='Search' value={text} onChange={e => setText(e.target.value)} />
         <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
             <option value="usd">USD</option>
             <option value="eur">EUR</option>
             <option value="jpy">JPY</option>
         </select>
-          <div>
+       {(!!coins.length || isLoading)&& (   <div className={styles.searchResult}>
             {isLoading && <RotatingLines width='50px' height='50px' strokeWidth='2' strokeColor='#3874ff'/>}
             <ul>
                 {coins.map(coin => <li key={coin.id}>
@@ -63,7 +64,7 @@ function Search({currency, setCurrency}) {
                     <p>{coin.name}</p>
                 </li>)}
             </ul>
-          </div>
+          </div>)}
     </div>
   )
 }
