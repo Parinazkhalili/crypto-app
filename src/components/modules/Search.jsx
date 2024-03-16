535cc584f4bc2a8132d6e5a434cc5f609940de64
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-03-16 10:22:10 +0330

    Showing loading spinner during search

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,17 +1,21 @@
 
 import React, { useEffect, useState } from 'react'
 import { searchCoin } from '../../services/cryptoApi';
+import { RotatingLines } from 'react-loader-spinner';
 
 function Search({currency, setCurrency}) {
     const [text, setText] = useState("");
-    const [coins, setCoins] = useState([])
+    const [coins, setCoins] = useState([]);
+    const [isLoading , setIsLoading] = useState(false);
    
 
     useEffect(() => {
         const controller = new AbortController();
 
         setCoins([]);
-        if(!text) return;
+        if(!text){
+            setIsLoading(false);
+            return;} 
 
     
         const search = async () => {
@@ -20,9 +24,13 @@ function Search({currency, setCurrency}) {
                 const res = await fetch(searchCoin(text), {signal: controller.signal});
             const json = await res.json();
             console.log(json)
-           if (json.coins) 
-           { setCoins(json.coins)  } else 
-           {alert(json.status.error_message);}
+           if (json.coins) { 
+
+            setIsLoading(false); 
+            setCoins(json.coins)  }
+
+            else {alert(json.status.error_message);}
+
             } catch (error) {
             
                 if (error.name !== "AbortError" ) {
@@ -34,11 +42,12 @@ function Search({currency, setCurrency}) {
           
        
         }
+       setIsLoading(true);
 
        search();
         return () => controller.abort() ;
     }, [text])
-  return (
+     return (
     <div>
         < input type="text" placeholder='Search' value={text} onChange={e => setText(e.target.value)} />
         <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
@@ -47,6 +56,7 @@ function Search({currency, setCurrency}) {
             <option value="jpy">JPY</option>
         </select>
           <div>
+            {isLoading && <RotatingLines width='50px' height='50px' strokeWidth='2' strokeColor='#3874ff'/>}
             <ul>
                 {coins.map(coin => <li key={coin.id}>
                     <img  src={coin.thumb} alt={coin.name}/>
