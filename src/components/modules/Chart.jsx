9aab7fbc9dc4017185a0a9d1bfaacf254ba19078
Parinaz KHalili <parinazkhalili27@gmail.com>
2024-04-03 18:54:45 +0330

    Ability to change the chart by the user

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -12,6 +12,13 @@ function Chart({chart, setChart}) {
   console.log(chart)
   const [type , setType]= useState("prices")
   console.log(convertData(chart, type))
+
+  const typeHandler = event =>{
+    if (event.target.tagName=== "BUTTON") {
+      const type = event.target.innerText.toLowerCase().replace(" ", "_")
+      setType(type)
+    }
+  }
   return (
     <div className={styles.container}>
       <span className={styles.cross} onClick={() => setChart(null)}>X</span>
@@ -23,10 +30,10 @@ function Chart({chart, setChart}) {
         <div className={styles.graph}>
          <ChartComponent data={convertData(chart, type)}  type={type}/>
         </div>
-        <div className={styles.types}>
-          <button>Prices</button>
-          <button>Market Caps</button>
-          <button>Total Volumes</button>
+        <div className={styles.types} onClick={typeHandler}>
+          <button className={type==="prices" ? styles.selected : null}>Prices</button>
+          <button  className={type==="market_caps" ? styles.selected : null}>Market Caps</button>
+          <button  className={type==="total_volumes" ? styles.selected : null}>Total Volumes</button>
         </div>
         <div className={styles.details}>
           <div><p>Prices:</p>
